e3058b5172759e36f804790d9f8e3c3ae24531c1
Travis Holt <Travisholt92@gmail.com>
2021-10-22 15:15:05 -0400

    KangOS:  soong: Dump TARGET_CAMERA_NEEDS_CLIENT_INFO
    https://github.com/AOSP-Krypton/vendor_krypton/commit/616264ff6d430b3632563510e7e43e19a2b2d32c

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -39,7 +39,8 @@ SOONG_CONFIG_kangosGlobalVars += \
     target_ld_shim_libs \
     target_process_sdk_version_override \
     target_surfaceflinger_udfps_lib \
-    uses_camera_parameter_lib
+    uses_camera_parameter_lib \
+    camera_needs_client_info
 
 SOONG_CONFIG_NAMESPACES += kangosNvidiaVars
 SOONG_CONFIG_kangosNvidiaVars += \
@@ -65,6 +66,7 @@ endif
 # Soong bool variables
 SOONG_CONFIG_kangosGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
 SOONG_CONFIG_kangosGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
+SOONG_CONFIG_kangosGlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
 SOONG_CONFIG_kangosGlobalVars_ignores_ftp_pptp_conntrack_failure := $(TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)
 SOONG_CONFIG_kangosGlobalVars_needs_netd_direct_connect_rule := $(TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE)
 SOONG_CONFIG_kangosNvidiaVars_uses_nv_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
